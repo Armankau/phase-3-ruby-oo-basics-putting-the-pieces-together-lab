ffe9fc886ed0a130b0ff33979a968b904050a901
+ <+>
2022-11-12 12:16:49 -0800

    commit tree cc6f1f007393cc1d085299554896f3f741b93150

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+    attr_reader :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+    end
+    attr_writer :author, :page_count, :genre
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
